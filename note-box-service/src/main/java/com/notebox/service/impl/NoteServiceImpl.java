e3bfdab9315756153aa9c8f168202cc000fd1e7e
Marcin Mikołajczyk <marcin.mikolajczyk22@gmail.com>
2023-09-23 01:26:00 +0200

    #6 Added storage strategy for notes and integration tests

--- note-box-service/src/main/java/com/notebox/service/impl/NoteServiceImpl.java
+++ note-box-service/src/main/java/com/notebox/service/impl/NoteServiceImpl.java
@@ -1,44 +1,52 @@
 package com.notebox.service.impl;
 
 import com.notebox.model.Note;
+import com.notebox.repository.NoteRepository;
 import com.notebox.service.NoteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class NoteServiceImpl implements NoteService {
+    private final NoteRepository noteRepository;
+
     @Override
     public Note createNote(String title, String content) {
-        return buildNote(title, content);
+        return noteRepository.save(Note.builder()
+                .title(title)
+                .content(content)
+                .date(LocalDateTime.now())
+                .build());
     }
 
     @Override
     public Note updateNote(Long id, String title, String content) {
-        return null;
+        return noteRepository.save(Note.builder()
+                .id(id)
+                .title(title)
+                .content(content)
+                .date(LocalDateTime.now())
+                .build());
     }
 
     @Override
     public Long deleteNote(Long id) {
-        return null;
+        noteRepository.deleteById(id);
+        return id;
     }
 
     @Override
     public List<Note> getAllNotes() {
-        return null;
+        return noteRepository.findAll();
     }
 
     @Override
     public List<Note> getAllNotesByTitle(String title) {
-        return null;
+        return noteRepository.findByTitleContaining(title);
     }
 
-    private Note buildNote(String title, String content){
-        return Note.builder()
-                .title(title)
-                .content(content)
-                .date(LocalDateTime.now())
-                .build();
-    }
 }
---
+++ note-box-service/src/test/java/integr/NoteControllerIntegrationTest.java
@@ -0,0 +1,139 @@
+package integr;
+
+import com.notebox.NoteBoxServiceApplication;
+import com.notebox.repository.NoteRepository;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = NoteBoxServiceApplication.class)
+@SqlGroup({
+        @Sql(value = "classpath:queries/clear-all-notes.sql", executionPhase = BEFORE_TEST_METHOD),
+        @Sql(value = "classpath:queries/prepare-test-notes.sql", executionPhase = BEFORE_TEST_METHOD)
+})
+class NoteControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private NoteRepository noteRepository;
+
+    @Test
+    void createNoteShouldReturnNoteWithCreatedStatus() throws Exception {
+        // given
+        String title = "example";
+        String content = "This is example note content";
+
+        // when,then
+        mockMvc.perform(post("http://localhost:6000/v1/api/note/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                        .param("title", title)
+                        .param("content", content))
+                .andExpect(jsonPath("$.title").value(title))
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        assertThat(noteRepository.findAll()).hasSize(6);
+    }
+
+    @Test
+    void createNoteShouldReturnBadRequestStatusFotNotValidTitle() throws Exception {
+        // given
+        String title = "az12345Z";
+        String content = "This is example note content";
+
+        // when,then
+        mockMvc.perform(post("http://localhost:6000/v1/api/note/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("title", title)
+                        .param("content", content))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNoteShouldReturnBadRequestStatusFotNotValidContent() throws Exception {
+        // given
+        String title = "example";
+        String content = "This is e@xample not#e content with special $ characters";
+
+        // when,then
+        mockMvc.perform(post("http://localhost:6000/v1/api/note/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("title", title)
+                        .param("content", content))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateNoteShouldReturnUpdatedNoteFromDataBase() throws Exception {
+        // given
+        String title = "updatedTitle";
+        String content = "example updated content";
+
+        // when,then
+        mockMvc.perform(put("http://localhost:6000/v1/api/note/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("id", String.valueOf(102))
+                        .param("title", title)
+                        .param("content", content))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(102))
+                .andExpect(jsonPath("$.title").value(title))
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(status().isOk());
+        assertThat(noteRepository.findAll()).hasSize(5);
+        assertEquals(noteRepository.findById(102L).orElseThrow().getTitle(), title);
+    }
+
+    @Test
+    void deleteNoteShouldRemoveNoteFromDataBase() throws Exception {
+        // given, when,then
+        mockMvc.perform(delete("http://localhost:6000/v1/api/note/delete")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("id", String.valueOf(4)))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("4"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAllNotesShouldReturnFiveRecordsFromDataBase() throws Exception {
+        // given, when, then
+        mockMvc.perform(get("http://localhost:6000/v1/api/note/get/all")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", Matchers.hasSize(5)));
+    }
+
+    @Test
+    void getAllNotesByTitleShouldReturnRecordWithMatchingTitles() throws Exception {
+        // given
+        String title = "DEV";
+
+        // when, then
+        mockMvc.perform(get("http://localhost:6000/v1/api/note/get/by/title")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("title", title))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", Matchers.hasSize(3)));
+    }
+
+}
